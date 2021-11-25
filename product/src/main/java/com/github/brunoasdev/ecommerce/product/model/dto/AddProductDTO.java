3ecb5930c136037bdd9ce1a7fe44b018ce3e1c03
brunoasdev <balmeida80@gmail.com>
2021-11-25 13:12:34 -0300

    POJO AddProductDTO added

---
+++ product/src/main/java/com/github/brunoasdev/ecommerce/product/model/dto/AddProductDTO.java
@@ -0,0 +1,29 @@
+package com.github.brunoasdev.ecommerce.product.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.math.BigDecimal;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class AddProductDTO {
+
+    @NotEmpty(message = "Name is mandatory.")
+    @Size(min = 3, max = 30, message = "Minimum {min} and maximum {max} characters.")
+    private String name;
+
+    @NotNull(message = "Price is mandatory.")
+    private BigDecimal price;
+
+    @NotNull(message = "Quantity is mandatory.")
+    private Integer quantity;
+
+}
