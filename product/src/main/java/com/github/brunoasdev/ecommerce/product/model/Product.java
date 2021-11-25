3fc1f04890d5f93199079cebed54a096f80eae08
brunoasdev <balmeida80@gmail.com>
2021-11-25 12:49:22 -0300

    Product model added

---
+++ product/src/main/java/com/github/brunoasdev/ecommerce/product/model/Product.java
@@ -0,0 +1,42 @@
+package com.github.brunoasdev.ecommerce.product.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.elasticsearch.annotations.Document;
+import org.springframework.data.elasticsearch.annotations.Field;
+import org.springframework.data.elasticsearch.annotations.FieldType;
+
+import javax.validation.constraints.Min;
+import java.math.BigDecimal;
+import java.time.LocalDate;
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Document(indexName = "product")
+public class Product {
+
+    @Id
+    @Field(type = FieldType.Keyword)
+    private String id;
+
+    @Field(type = FieldType.Text)
+    private String name;
+
+    @Field(type = FieldType.Double)
+    private BigDecimal price;
+
+    @Min(value = 0, message = "Minimum quantity is 0.")
+    @Field(type = FieldType.Integer)
+    private Integer quantity;
+
+    @CreatedDate
+    @Field(type = FieldType.Date)
+    private LocalDate created;
+
+}
